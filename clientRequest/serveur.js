ec5c64cc090343728b27a41b0e1e68bcc17a88f5
HeidmannPaul <paulheid@hotmail.fr>
2018-01-26 22:12:04 +0100

    Add relation type in database and requestbytype

--- clientRequest/serveur.js
+++ clientRequest/serveur.js
@@ -15,10 +15,10 @@ var cheerio = require("cheerio");
 var keyFileStorage = require("key-file-storage");
 
 /**
- * ATTENTION A BIEN SEPCIFIER LE BON CHEMIN
+ * ATTENTION A BIEN SPECIFIER LE BON CHEMIN
  */
 //var kfs = keyFileStorage("/Users/sylvinho/Documents/M2/EAppJDM/Cache", true);
-var kfs = keyFileStorage(__dirname.toString()+"Cache", true);
+var kfs = keyFileStorage(__dirname.toString() + "Cache", true);
 
 var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"]
 var nbMaxFiles = 2000;
@@ -29,7 +29,7 @@ var nbMaxFiles = 2000;
 
 
 if (!kfs['compteur']) {
-    
+
     console.log("Création du compteur");
     kfs['compteur'] = 0;
 }
@@ -43,18 +43,6 @@ function emitRequest(givenUri, callback) {
             var body = iconv.decode(html, PAGE_ENCODING);
             var $ = cheerio.load(body, { decodeEntities: false });
             let CODE = $('def');
-            //console.log("$", $.html())
-            //on peut aussi remplacer CODE par $('def') ci-dessous
-            /*CODE.each(function()
-            {
-               // console.log("size",$(this).find('br').length);
-               let rbr= $(this).find('br').remove();
-                console.log(rbr.html())
-            })*/
-
-            /*console.log(CODE.html())
-            let CODE2= CODE.remove('br').html();
-            console.log(CODE2);*/
 
             return callback($.html());
         }
@@ -167,13 +155,17 @@ app.get("/:value", function (req, res) {
 
 });
 
+
+
+
 MongoClient.connect(url, function (err, db) {
+
 
     //ne pas se référer au projet vente en ligne pour la ligne ci dessous
     /*
     In mongodb version >= 3.0, That database variable is actually the parent object 
     of the object you are trying to access with database.collection('whatever').
-     To access the correct object, you need to reference your database name
+    To access the correct object, you need to reference your database name
     */
     let database = db.db("JeuxDeMots");
     assert.equal(null, err);
@@ -202,8 +194,151 @@ MongoClient.connect(url, function (err, db) {
             }
         });
     });
+
+    app.get("/Rel/:val", function (req, res) {
+        let value = req.params.val;
+        //var cursor = database.collection("words").find().sort({"coeff":-1}).limit(5);
+        var cursor = database.collection("relation").find({ rel_name: { $regex: "^" + value, $options: "i" } }).sort({ "rel_id": 1 }).limit(5);
+        var resultat = [];
+        cursor.each(function (err, doc) {
+            assert.equal(null, err);
+            if (doc != null) {
+                resultat.push(doc);
+            }
+            else {
+                var json = JSON.stringify(resultat);
+                //console.log("res :");
+                //console.log(""+resultat);
+                //console.log("fin res");
+
+                res.setHeader('Acces-Control-Allow-Origin', '*');
+                res.setHeader("Content-type", "application/json ; charset=UTF-8");
+                res.end(json);
+            }
+        });
+    });
+
+
+    app.get("/id/:value/Rel/:rel", function (req, res) {
+
+        let value = req.params.value;
+        let relationName = req.params.rel;
+        let relationNames = relationName.split(",");
+        console.log(relationNames);
+
+        //les mois vont de 0 à 11
+        let d = new Date();
+
+        let finalValue = "";
+        let values = value.split(" ")
+        if (values.length > 1) {
+            finalValue = values[0];
+
+            for (let i = 1; i < values.length; i++) {
+                finalValue += "+" + values[i];
+            }
+        }
+        else {
+            finalValue = value;
+        }
+
+        res.setHeader('Acces-Control-Allow-Origin', '*');
+
+
+        // res.setHeader("Content-type", "text/plain ; charset=ISO-8859-1");
+
+
+        if (relationNames.length == 1) {
+            let uri = "http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" + finalValue + "&rel=" + relationNames[0];
+            console.log("valeur uri ", uri);
+            emitRequest(uri, function (body) {
+                res.end(body);
+            });
+        } else {
+            let resbody = "";
+            let i = 0;
+            relationNames.forEach(element => {
+                let uri = "http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" + finalValue + "&rel=" + element;
+                console.log("valeur uri ", uri)
+                emitRequest(uri, function (body) {
+                    console.log("VALEUR DE body "/* + body*/)
+                    if (i == 0) {
+                        resbody += body;
+                    } else {
+                        resbody = properResponse(resbody, body);
+                    }
+                    if (i == relationNames.length - 1) {
+                        console.log("RETOUR",resbody);
+                        res.end(resbody);
+                    }
+                    i++;
+                });
+
+            })
+        }
+    });
+
+
+
 });
-//emitRequest();
+
+function properResponse(resbody, body) {
+    let resultat = "";
 
 
+    let def = resbody.split("</def>");
+    if (def.length > 1) {
+        resultat += def[0] + "</def>";
+        let allRelations = def[1];
+        let firstPart = allRelations.split("// les types de relations (Relation Types) : rt;rtid;'trname';'trgpname';'rthelp'");
+      
+        let relationsTypes = "";
+        let entranteSortante;
+        let entrante;
+        if (firstPart) {
+            if (firstPart[0]) {
+                resultat+=firstPart[0];
+            } if (firstPart[1]) {
+                relationsTypes = firstPart[1].split("// les relations sortantes : r;rid;node1;node2;type;w");
+                entranteSortante=relationsTypes[1].split("// les relations entrantes : r;rid;node1;node2;type;w");
+                entrante=entranteSortante[1].split("// END");
+             //   console.log("relationsTypes", relationsTypes)
+            }
+        }
 
+        let def2 = body.split("</def>");
+        if (def2.length > 1) {
+            let allRelations2 = def2[1];
+            let firstPart2 = allRelations2.split("// les types de relations (Relation Types) : rt;rtid;'trname';'trgpname';'rthelp'");
+            let entities = "";
+            let relationsTypes2 = "";
+            let entranteSortante2 = "";
+            let entrante2;
+            if (firstPart2) {
+                if (firstPart2[0]) {
+                    entities = firstPart2[0].split("// les noeuds/termes (Entries) : e;eid;'name';type;w;'formated name'");
+                    if (entities[1]) {
+                        resultat+= entities[1];
+                      }
+                 //   console.log("entities", entities)
+                }
+                
+
+                if (firstPart2[1]) {
+                    relationsTypes2 = firstPart2[1].split("// les relations sortantes : r;rid;node1;node2;type;w");
+                    entranteSortante2=relationsTypes2[1].split("// les relations entrantes : r;rid;node1;node2;type;w");
+                    entrante2=entranteSortante2[1].split("// END");
+                   // console.log("relationsTypes", relationsTypes2);
+                }
+                
+            }
+            resultat+="// les types de relations (Relation Types) : rt;rtid;'trname';'trgpname';'rthelp'"+relationsTypes[0];
+            resultat+=relationsTypes2[0]+"// les relations sortantes : r;rid;node1;node2;type;w";
+            resultat+=entranteSortante[0]+entranteSortante2[0]+"// les relations entrantes : r;rid;node1;node2;type;w"+entrante[0]+entrante2[0]+"// END"+entrante[1];
+        }
+    }
+
+
+    // console.log("res",resultat);
+    return resultat;
+}
